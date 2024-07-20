4d9c13651068adf0ccaa22e7daaeebf19c9e39a1
Shanzeye <165150162+Shanzeye@users.noreply.github.com>
2024-07-20 23:13:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+class Person {
+    _personality;
+    constructor() {
+        this._personality = "Mystery";
+    }
+    userInput(input) {
+        if (input.includes("Talk to other about yourself openly")) {
+            this._personality = "Talk happly";
+        }
+        else if (input.includes("Keep your word to yourself")) {
+            this._personality = "Talk rude";
+        }
+    }
+    get personality() {
+        return this._personality;
+    }
+}
+class Main {
+    async main() {
+        const ans = await inquirer.prompt({
+            type: "list",
+            name: "choice",
+            message: chalk.bgRed("Tell your behaviour ?"),
+            choices: [
+                "1: Talk to others about my self",
+                "2: Keep quite & don't ask questions"
+            ]
+        });
+        let myPerson = new Person();
+        myPerson.userInput(ans.choice);
+        console.log(`you are ${myPerson.personality}`);
+    }
+}
+const myObject = new Main();
+myObject.main();
